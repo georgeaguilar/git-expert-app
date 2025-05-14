28dd4a8909bb028f3666c35f81c22d4b68620ff7
Jorge Aguilar <georgeaguilar11@hotmail.com>
2025-05-14 09:29:20 -0600

    feat: add category

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
 function App() {
+  const [categories, setCategories] = useState([
+    'Dragon Ball Z',
+    'Jujutsu Kaisen',
+  ]);
+
+  function addCategory() {
+    setCategories((cat) => ['Marvel', ...cat]);
+  }
+
   return (
     <>
       <div>GitExpertApp</div>
+      <button onClick={addCategory}>agregar</button>
+      <ol>
+        {categories.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ol>
     </>
   );
 }
